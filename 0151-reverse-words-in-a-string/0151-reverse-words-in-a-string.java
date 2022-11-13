789d12864510038f783379c58638763c739bba99
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-13 00:55:04 -0800

    Time: 17 ms (50.22%), Space: 44.1 MB (41.00%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,3 +1,23 @@
+class Solution{
+    public String reverseWords(String s){
+        String strs[] = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i=strs.length-1; i>=0; i--){
+            sb.append(strs[i]+" ");
+        }
+        return sb.toString().trim();
+    }
+}
+
+
+
+
+
+
+
+
+/*
+util.optional class usage
 class Solution 
 {
     public String reverseWords(String s) 
@@ -6,4 +26,4 @@ class Solution
                      .reduce((w1, w2) -> w2 + " " + w1)
                      .orElse("");
     }
-}
+}*/
