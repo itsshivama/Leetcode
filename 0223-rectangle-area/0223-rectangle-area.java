75f290bb2f883fab57c88038bcc720e5da67f7b6
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-16 23:33:19 -0800

    Time: 4 ms (81.38%), Space: 43.7 MB (15.86%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -1,30 +1,13 @@
 class Solution {
-    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int areaOfA = (ay2 - ay1) * (ax2 - ax1);
-        int areaOfB = (by2 - by1) * (bx2 - bx1);
-
-        // calculate x overlap
-        int left = Math.max(ax1, bx1);
-        int right = Math.min(ax2, bx2);
-        int xOverlap = right - left;
-
-        // calculate y overlap
-        int top = Math.min(ay2, by2);
-        int bottom = Math.max(ay1, by1);
-        int yOverlap = top - bottom;
-
-        int areaOfOverlap = 0;
-        // if the rectangles overlap each other, then calculate
-        // the area of the overlap
-        if (xOverlap > 0 && yOverlap > 0) {
-            areaOfOverlap = xOverlap * yOverlap;
-        }
-
-        // areaOfOverlap is counted twice when in the summation of
-        // areaOfA and areaOfB, so we need to subtract it from the
-        // total, to get the toal area covered by both the rectangles
-        int totalArea = areaOfA + areaOfB - areaOfOverlap;
-
-        return totalArea;
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+    {
+        int area1=Math.abs(ax1-ax2)*Math.abs(ay1-ay2);
+        int area2=Math.abs(bx1-bx2)*Math.abs(by1-by2);
+        int x1=Math.min(ax2,bx2)-Math.max(ax1,bx1);
+        int y1=Math.min(ay2,by2)-Math.max(ay1,by1);
+        int repeat=0;
+        if(x1>0 && y1>0)
+            repeat=x1*y1;
+        return area1+area2-repeat;
     }
 }
