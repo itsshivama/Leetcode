94564dbbf23ce3e07287fc8151286ac751516b04
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-19 03:43:03 -0800

    Time: 29 ms (72.41%), Space: 54.9 MB (22.41%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.java
+++ 0587-erect-the-fence/0587-erect-the-fence.java
@@ -1,104 +1,49 @@
-class Solution {
-public int[][] outerTrees(int[][] trees) {
-        List<int[]> ps = new ArrayList<>(trees.length);
-        Collections.addAll(ps, trees);
-        var out = ConvexHull.GrahamScan(ps,i->i[0],i->i[1]);
-        int[][] ans = new int[out.size()][2];
-        for(int i = 0; i < out.size(); i++){
-            ans[i] = out.get(i);
+import java.util.Arrays;
+
+public class Solution {
+    private static final int MAX = 100;
+
+    public int[][] outerTrees(int[][] trees) {
+        int n = trees.length;
+        if (n <= 2) {
+            return trees;
         }
-        return ans;
+        radixSort2D(trees);
+        int[][] st = new int[n * 2][];
+        int idx = 0;
+        for (int[] t : trees) {
+            while (idx > 1 && polarOrder(st[idx - 2], st[idx - 1], t) < 0) {
+                idx--;
+            }
+            st[idx++] = t;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (idx > 1 && polarOrder(st[idx - 2], st[idx - 1], trees[i]) < 0) {
+                idx--;
+            }
+            st[idx++] = trees[i];
+        }
+        return Arrays.stream(st, 0, idx).distinct().toArray(int[][]::new);
     }
 
-   public static class ConvexHull {
-        public static <E> List<E> GrahamScan(
-                List<E> points,
-                ToDoubleFunction<E> getX,
-                ToDoubleFunction<E> getY) {
-            if (points.size() <= 3) {
-                return points;
+    private void radixSort2D(int[][] trees) {
+        int[][] aux = new int[trees.length][];
+        for (int p = 1; p >= 0; p--) {
+            int[] count = new int[MAX + 2];
+            for (int[] t : trees) {
+                count[t[p] + 1]++;
             }
-            var start = points.get(0);
-            int start_idx = 0;
-            for (var i = 0; i < points.size(); i++) {
-                var p = points.get(i);
-                if (getY.applyAsDouble(start) > getY.applyAsDouble(p)) {
-                    start = p;
-                    start_idx = i;
-                }
+            for (int c = 0; c <= MAX; c++) {
+                count[c + 1] += count[c];
             }
-
-            final var final_start = start;
-            swap(points,start_idx, points.size()-1);
-            points.remove(points.size()-1);
-            points.sort((a, b) -> {
-                var comp_a = ccw(final_start,a,b,getX,getY);
-                if (comp_a == 0) {
-                    var comp_d = distance(a, final_start, getX, getY) - distance(b, final_start, getX, getY);
-                    return comp_d < 0 ? -1 : comp_d > 0 ? 1 : 0;
-                }
-                else return -comp_a < 0 ? -1 : -comp_a > 0 ? 1 :0;
-            });
-            List<E> temp = new ArrayList<>(points.size()+1);
-            temp.add(start);
-            temp.addAll(points);
-            points = temp;
-            int s = points.size() - 1;
-            while (s >= 0 && ccw(start, points.get(s), points.get(points.size() - 1), getX, getY) == 0) {
-                s--;
+            for (int[] t : trees) {
+                aux[count[t[p]]++] = t;
             }
-            s++;
-
-            for (int e = points.size() - 1; s < e; e--, s++) {
-                swap(points, s, e);
-            }
-
-            Deque<E> points_stack = new ArrayDeque<>();
-            
-            points_stack.addLast(points.get(0));
-points_stack.addLast(points.get(1));
-
-            for (int i = 2; i < points.size(); i++) {
-                var p = points.get(i);
-                var last = points_stack.removeLast();
-                while (!points_stack.isEmpty() && ccw(points_stack.getLast(), last, p, getX, getY) < 0) {
-                    last = points_stack.removeLast();
-                }
-                points_stack.addLast(last);
-                points_stack.addLast(p);
-            }
-
-            List<E> res = new ArrayList<>(points_stack.size());
-            res.addAll(points_stack);
-            return res;
+            System.arraycopy(aux, 0, trees, 0, trees.length);
         }
+    }
 
-        private static <E> void swap(List<E> points, int i, int j) {
-            var t = points.get(i);
-            points.set(i, points.get(j));
-            points.set(j, t);
-        }
-
-        private static <E> double distance(E a, E b, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
-            double ax = getX.applyAsDouble(a);
-            double ay = getY.applyAsDouble(a);
-
-            double bx = getX.applyAsDouble(b);
-            double by = getY.applyAsDouble(b);
-            return Math.pow(ax - bx, 2) + Math.pow(ay - by, 2);
-        }
-
-        private static <E> double ccw(E a, E b, E c, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
-            double ax = getX.applyAsDouble(a);
-            double ay = getY.applyAsDouble(a);
-
-            double bx = getX.applyAsDouble(b);
-            double by = getY.applyAsDouble(b);
-
-            double cx = getX.applyAsDouble(c);
-            double cy = getY.applyAsDouble(c);
-            // (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0]);
-            return (bx - ax) * (cy - by) - (by - ay) * (cx - bx);
-        }
+    private int polarOrder(int[] p, int[] q, int[] r) {
+        return (q[0] - p[0]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[0] - q[0]);
     }
 }
