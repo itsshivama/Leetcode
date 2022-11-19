2a89615da174ba382afc6b0607c8fda83d687ee5
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-19 03:42:32 -0800

    Time: 43 ms (44.83%), Space: 54.9 MB (25.86%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.java
+++ 0587-erect-the-fence/0587-erect-the-fence.java
@@ -1,53 +1,104 @@
 class Solution {
-    public int[][] outerTrees(int[][] trees) {
-        Arrays.sort(trees, (a, b) -> ((a[0] - b[0] != 0) ? (a[0] - b[0]) : (a[1] - b[1])));
-        int n = trees.length;
-
-        boolean[] isUsed = new boolean[n];
-        int[] hull = new int[n + 2];
-        Arrays.fill(isUsed, false);
-
-        int top = 0;
-
-        for (int i = 0; i < n; i++) {
-            
-            while (top >= 2 && getArea(trees[hull[top - 1]], trees[hull[top]], trees[i]) > 0) {
-                isUsed[hull[top--]] = false;
-            }
-
-            hull[++top] = i;
-            isUsed[i] = true;
+public int[][] outerTrees(int[][] trees) {
+        List<int[]> ps = new ArrayList<>(trees.length);
+        Collections.addAll(ps, trees);
+        var out = ConvexHull.GrahamScan(ps,i->i[0],i->i[1]);
+        int[][] ans = new int[out.size()][2];
+        for(int i = 0; i < out.size(); i++){
+            ans[i] = out.get(i);
         }
-
-        isUsed[0] = false;
-        for (int i = n - 1; i >= 0; i--) {
-
-            if (isUsed[i]) {
-                continue;
-            }
-
-            while (top >= 2 && getArea(trees[hull[top - 1]], trees[hull[top]], trees[i]) > 0) {
-                top--;
-            }
-
-            hull[++top] = i;
-        }
-
-        top--;
-
-        int[][] treesCoordinates = new int[top][2];
-        for (int i = 1; i <= top; i++) {
-            treesCoordinates[i - 1][0] = trees[hull[i]][0];
-            treesCoordinates[i - 1][1] = trees[hull[i]][1];
-        }
-
-        return (treesCoordinates);
-
+        return ans;
     }
 
-    public int getArea(int[] a, int[] b, int[] c) {
-        int x1 = b[0] - a[0], y1 = b[1] - a[1];
-        int x2 = c[0] - a[0], y2 = c[1] - a[1];
-        return (x1 * y2 - x2 * y1);
+   public static class ConvexHull {
+        public static <E> List<E> GrahamScan(
+                List<E> points,
+                ToDoubleFunction<E> getX,
+                ToDoubleFunction<E> getY) {
+            if (points.size() <= 3) {
+                return points;
+            }
+            var start = points.get(0);
+            int start_idx = 0;
+            for (var i = 0; i < points.size(); i++) {
+                var p = points.get(i);
+                if (getY.applyAsDouble(start) > getY.applyAsDouble(p)) {
+                    start = p;
+                    start_idx = i;
+                }
+            }
+
+            final var final_start = start;
+            swap(points,start_idx, points.size()-1);
+            points.remove(points.size()-1);
+            points.sort((a, b) -> {
+                var comp_a = ccw(final_start,a,b,getX,getY);
+                if (comp_a == 0) {
+                    var comp_d = distance(a, final_start, getX, getY) - distance(b, final_start, getX, getY);
+                    return comp_d < 0 ? -1 : comp_d > 0 ? 1 : 0;
+                }
+                else return -comp_a < 0 ? -1 : -comp_a > 0 ? 1 :0;
+            });
+            List<E> temp = new ArrayList<>(points.size()+1);
+            temp.add(start);
+            temp.addAll(points);
+            points = temp;
+            int s = points.size() - 1;
+            while (s >= 0 && ccw(start, points.get(s), points.get(points.size() - 1), getX, getY) == 0) {
+                s--;
+            }
+            s++;
+
+            for (int e = points.size() - 1; s < e; e--, s++) {
+                swap(points, s, e);
+            }
+
+            Deque<E> points_stack = new ArrayDeque<>();
+            
+            points_stack.addLast(points.get(0));
+points_stack.addLast(points.get(1));
+
+            for (int i = 2; i < points.size(); i++) {
+                var p = points.get(i);
+                var last = points_stack.removeLast();
+                while (!points_stack.isEmpty() && ccw(points_stack.getLast(), last, p, getX, getY) < 0) {
+                    last = points_stack.removeLast();
+                }
+                points_stack.addLast(last);
+                points_stack.addLast(p);
+            }
+
+            List<E> res = new ArrayList<>(points_stack.size());
+            res.addAll(points_stack);
+            return res;
+        }
+
+        private static <E> void swap(List<E> points, int i, int j) {
+            var t = points.get(i);
+            points.set(i, points.get(j));
+            points.set(j, t);
+        }
+
+        private static <E> double distance(E a, E b, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
+            double ax = getX.applyAsDouble(a);
+            double ay = getY.applyAsDouble(a);
+
+            double bx = getX.applyAsDouble(b);
+            double by = getY.applyAsDouble(b);
+            return Math.pow(ax - bx, 2) + Math.pow(ay - by, 2);
+        }
+
+        private static <E> double ccw(E a, E b, E c, ToDoubleFunction<E> getX, ToDoubleFunction<E> getY) {
+            double ax = getX.applyAsDouble(a);
+            double ay = getY.applyAsDouble(a);
+
+            double bx = getX.applyAsDouble(b);
+            double by = getY.applyAsDouble(b);
+
+            double cx = getX.applyAsDouble(c);
+            double cy = getY.applyAsDouble(c);
+            // (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0]);
+            return (bx - ax) * (cy - by) - (by - ay) * (cx - bx);
+        }
     }
 }
