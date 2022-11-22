19daf90b51edb96646a12ee6cb11442bdced689b
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-21 21:45:42 -0800

    Time: 121 ms (35.09%), Space: 43.5 MB (41.10%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,17 +1,23 @@
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            dp[i] = i;
-
-            for (int j = 1; j * j <= i; j++) {
-                int square = j * j;
-                dp[i] = Math.min(dp[i], 1 + dp[i - square]);
-            }
+        int[] memo = new int[n + 1];
+        return helper(n, memo);
+    }
+    
+    public int helper(int n, int[] memo) {
+        if (n < 4)
+            return n;
+        
+        if (memo[n] != 0)
+            return memo[n];
+        
+        int ans = n;
+        
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            ans = Math.min(ans, 1 + helper(n - square, memo));
         }
-
-        return dp[n];
+        
+        return memo[n] = ans;
     }
 }
