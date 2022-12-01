5e52e15f5b2f99b71cb45660ea147a00665918f6
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-11-30 20:49:44 -0800

    Time: 2 ms (99.29%), Space: 42.8 MB (17.58%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,27 +1,26 @@
 class Solution {
-    public boolean halvesAreAlike(String str) {
-        int n=str.length();
-        int count=0;
-        for(int i=0;i<n/2;i++){
-            if(isVowel(str.charAt(i))){
-                count++;
-            }
-        }
-        for(int i=n/2;i<n;i++){
-            if(isVowel(str.charAt(i))){
-                count--;
-            }
-        }
-        if(count==0){
+    public boolean halvesAreAlike(String s) {
+       char[] ch=s.toCharArray();
+       int len=s.length();
+       int mid=len/2;
+       int l=check(ch,0,mid-1);
+       int r=check(ch,mid,len-1);
+        if(l==r)
+        {
             return true;
         }
         return false;
+        
     }
-    public boolean isVowel(char ch){
-        if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u'
-            ||ch=='A'||ch=='E' || ch=='I' || ch=='O' || ch=='U'){
-                return true;
+    public int check(char[] ch,int start,int end){
+        int count=0;
+        for(int i=start;i<=end;i++)
+        {
+            if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'||ch[i]=='A'||ch[i]=='E'||ch[i]=='I'||ch[i]=='O'||ch[i]=='U')
+            {
+               count++; 
             }
-            return false;
+         }
+        return count;
     }
 }
