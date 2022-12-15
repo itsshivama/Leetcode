508944264e4819cf5df8f31bd1f2fd084be65078
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-12-14 19:40:37 -0800

    Time: 3 ms (100.00%), Space: 40.3 MB (99.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,39 @@
 class Solution {
-    int dp[][]=new int[1000+1][1000+1] ;
-    public int longestCommonSubsequence(String a, String b) {
-        int l1=a.length(),l2=b.length();
-        
-        for(int i=0;i<=l1;i++)
-        dp[i][0]=0;
-        
-        for(int i=0;i<=l2;i++)
-        dp[0][i]=0;
-        
-        for(int i=1;i<=l1;i++)
-        {
-            for(int j=1;j<=l2;j++)
-            {
-                if(a.charAt(i-1)==b.charAt(j-1))
-                dp[i][j]=1+dp[i-1][j-1];
-                
-                else dp[i][j]=Math.max(dp[i-1][j] , dp[i][j-1]);
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] arr1;
+        char[] arr2;
+        if (text1.length() > text2.length()) {
+            arr1 = text1.toCharArray();
+            arr2 = text2.toCharArray();
+        }
+        else {
+            arr1 = text2.toCharArray();
+            arr2 = text1.toCharArray();
+        }
+        int[] dp = new int[arr2.length+1];
+        for (int i=arr1.length-1; i>=0; i--) {
+            final char v1 = arr1[i];
+            for (int j=arr2.length-1, p=0, t; j>=0; j--) {
+                if ( v1 == arr2[j] ) {
+                    if (p == dp[j]) 
+                        dp[j]++;
+                    else if (p > dp[j]) {
+                        t = p+1;
+                        p = dp[j];
+                        dp[j] = t;
+                    }
+                    else p = dp[j];
+                }
+                else {
+                    p = dp[j];
+                    if (dp[j+1] > dp[j])
+                        dp[j] = dp[j+1];
+                }
             }
         }
-        return dp[l1][l2];
+        
+        return dp[0];
     }
-};
+    
+}
