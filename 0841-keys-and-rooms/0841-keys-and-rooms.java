effe0eec3935345318b230dbfb222b7fc81d3ab3
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-12-19 23:11:14 -0800

    Time: 2 ms (70.49%), Space: 42 MB (89.72%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,20 +1,22 @@
 class Solution {
-    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] visited = new boolean[rooms.size()];
-        int count = 0;
-        count = dfs(rooms,0, visited, count);
-        return count == rooms.size();
-    }
-
-    public int dfs(List<List<Integer>> rooms, int currRoom, boolean[] visited, int count) {
-        if (visited[currRoom] == true) {
-            return count;
+    public boolean canVisitAllRooms(List<List<Integer>> list) {
+        int n = list.size();
+        boolean [] vis = new boolean[n];
+        vis[0] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        int a;
+        int tot = n-1;
+        while (!q.isEmpty()){
+            a = q.remove();
+            for (int i = 0; i < list.get(a).size(); i++) {
+                if(!vis[list.get(a).get(i)]) {
+                    vis[list.get(a).get(i)] = true;
+                    q.add(list.get(a).get(i));
+                    tot--;
+                }
+            }
         }
-        visited[currRoom] = true;
-        ++count;
-        for (Integer roomKey: rooms.get(currRoom)) {
-            count = dfs(rooms, roomKey, visited, count);
-        }
-        return count;
+        return tot == 0;
     }
 }
