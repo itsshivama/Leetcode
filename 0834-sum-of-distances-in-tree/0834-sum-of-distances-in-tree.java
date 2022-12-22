ebf5000e13fb6c132977ea58675327fc043c47cf
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-12-22 04:59:24 -0800

    Time: 11 ms (100.00%), Space: 56.6 MB (98.56%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -1,39 +1,61 @@
 class Solution {
-    int[] ans, count;
-    List<Set<Integer>> graph;
-    int N;
-    public int[] sumOfDistancesInTree(int N, int[][] edges) {
-        this.N = N;
-        graph = new ArrayList<Set<Integer>>();
-        ans = new int[N];
-        count = new int[N];
-        Arrays.fill(count, 1);
-
-        for (int i = 0; i < N; ++i)
-            graph.add(new HashSet<Integer>());
-        for (int[] edge: edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        final int[] parents = buildParents(n, edges);
+        final int[] childrenCount = new int[n];
+        int rootDistance = 0;
+        for (int i = n - 1; i > 0; i--) {
+        	final int node = q[i];
+        	final int c = childrenCount[node] + 1;
+        	childrenCount[parents[node]] += c;
+        	rootDistance += c;
         }
-        dfs(0, -1);
-        dfs2(0, -1);
-        return ans;
+        final int root = q[0];
+        final int[] r = new int[n];
+        r[root] = rootDistance;
+        for (int i = 1; i < n; i++) {
+        	final int node = q[i];
+        	r[node] = r[parents[node]] + n - 2 * (childrenCount[node] + 1);
+        }
+        return r;
     }
-
-    public void dfs(int node, int parent) {
-        for (int child: graph.get(node))
-            if (child != parent) {
-                dfs(child, node);
-                count[node] += count[child];
-                ans[node] += ans[child] + count[child];
-            }
-    }
-
-    public void dfs2(int node, int parent) {
-        for (int child: graph.get(node))
-            if (child != parent) {
-                ans[child] = ans[node] - count[child] + N - count[child];
-                dfs2(child, node);
-            }
-    }
+	
+	static final int[] q = new int[30001];
+	
+	static int[] buildParents(int n, int[][] edges) {
+		final int[][] next = buildNext(n, edges);
+        final int[] parents = new int[n];
+        Arrays.fill(parents, -1);
+        int start = 0, end = 0;
+        q[end++] = 0;
+        while (start < end) {
+        	final int parent = q[start++];
+        	for (int c : next[parent]) {
+        		if (c != 0 && parents[c] < 0) {
+        			parents[c] = parent;
+        			q[end++] = c;
+        		}
+        	}
+        }
+        parents[0] = -1;
+        return parents;
+	}
+	
+	static int[][] buildNext(final int n, final int[][] edges) {
+		final int[] counts = new int[n];
+		for (final int[] e : edges) {
+			counts[e[0]]++;
+			counts[e[1]]++;
+		}
+		final int[][] res = new int[n][];
+		for (int i = 0; i < n; i++) {
+			res[i] = new int[counts[i]];
+		}
+		for (final int[] e : edges) {
+			final int l = e[0];
+			final int r = e[1];
+			res[l][--counts[l]] = r;
+			res[r][--counts[r]] = l;
+		}
+		return res;
+	}
 }
