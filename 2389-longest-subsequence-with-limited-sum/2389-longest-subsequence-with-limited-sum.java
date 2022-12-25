4c459e544988d894f2848223af321966efe42871
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-12-25 00:17:47 -0800

    Time: 3 ms (100.00%), Space: 43.3 MB (72.28%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
-        // Get the prefix sum array of the sorted 'nums'.
         Arrays.sort(nums);
-        for (int i = 1; i < nums.length; ++i)
-            nums[i] += nums[i - 1];
         
-        // For each query, find its insertion index to the prefix sum array.
-        int n = queries.length;
-        int answer[] = new int[n];
-        for (int i = 0; i < n; ++i) {
-            int index = binarySearch(nums, queries[i]);
-            answer[i] = index;
+        int n = nums.length;
+        int [] ans = new int [queries.length];
+        
+        for(int i=1; i<n; i++){
+            nums[i] += nums[i-1];
         }
         
-        return answer;
-    }
-    
-    int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target)
-                return mid + 1;
-            if (nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        for(int i=0; i<queries.length; i++){
+            
+            if(nums[n-1] <= queries[i]){
+                ans[i] = n;
+                continue;
+            }
+            
+            for(int j=0; j<n; j++){
+                if(nums[j] > queries[i]){
+                    ans[i] = j;
+                    break;
+                }
+            }
         }
-        return nums[left] > target ? left : left + 1;
+        
+        return ans;
     }
+
 }
