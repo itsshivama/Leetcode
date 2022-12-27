a2235a6335e94d8c7212c68ce69b10a93e203130
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2022-12-26 19:00:11 -0800

    Time: 9 ms (100.00%), Space: 52.7 MB (84.66%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,26 +1,20 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        int n = capacity.length, fullBags = 0;
-        
-        // Sort bags by the remaining capacity.
-        int[] remainingCapacity = new int[n];
-        for (int i = 0; i < n; ++i)
-            remainingCapacity[i] = capacity[i] - rocks[i];
-        Arrays.sort(remainingCapacity);
-    
-        // Iterate over sorted bags and fill them using additional rocks.
-        for (int i = 0; i < n; ++i) {
-            // If we can fill the current one, fill it and move on.
-            // Otherwise, stop the iteration.
-            if (additionalRocks >= remainingCapacity[i]) {
-                additionalRocks -= remainingCapacity[i];
-                fullBags++;
-            }
-            else
-                break;
+        long total = 0;
+        int length = capacity.length;
+        for(int i = 0; i < length; i++){
+            capacity[i] -= rocks[i];
+            total += capacity[i];
         }
-        
-        // Return `fullBags` after the iteration stops.
-        return fullBags;
+        if(total > additionalRocks){
+            Arrays.sort(capacity);
+            for(int i = 0; i < length; i++){
+                if(additionalRocks < capacity[i]){
+                    return i;
+                }
+                additionalRocks -= capacity[i];
+            }
+        }
+        return length;
     }
 }
