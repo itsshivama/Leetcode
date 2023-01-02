6b641a488c91f4f89cbc2f62f32b01ed854ab958
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-01-01 21:14:32 -0800

    Time: 1 ms (100.00%), Space: 40.7 MB (87.47%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,5 +1,18 @@
 class Solution {
+    public boolean myWay(String word)
+    {
+        //simply check the 3 conditions
+        int numCapital = 0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(Character.isUpperCase(word.charAt(i))) numCapital++;
+        }
+
+        return (numCapital == word.length() || (numCapital == 0) ||
+               (numCapital == 1 && Character.isUpperCase(word.charAt(0))));
+    }
+
     public boolean detectCapitalUse(String word) {
-        return word.matches("[A-Z]*|.[a-z]*");
+        return myWay(word);//Tc->o(n)
     }
 }
