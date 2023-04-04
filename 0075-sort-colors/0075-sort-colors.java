019fb07478d454388f2ff56d74a5a9ba42320ca5
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-04-04 12:21:19 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (75.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,11 +1,11 @@
 class Solution {
     public void sortColors(int[] nums) {
         int[] max = new int[3];
-        for(int i=0;i<nums.length;i++){
+        int i=0;
+        for(i=0;i<nums.length;i++){
             max[nums[i]]++;
         }
-        
-        int i=0;
+        i=0;
         for(int j=0;j<3;j++){
             while(max[j]>0){
                 nums[i++]=j;
