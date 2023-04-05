4654f403e3197ac2e219a48eea9d1f71269e59b3
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-04-05 13:09:44 +0530

    Time: 16 ms (31.54%), Space: 55.7 MB (100.00%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int minimizeArrayValue(int[] nums) {
-        
-        double max = nums[0];
-        double prefixSum = nums[0];
-        
-        
-        for (int i=1; i<nums.length; i++)
-        {
-            double curr = nums[i];
-            prefixSum += curr;
-            
-            if (curr > max)
-            {
-                max = Math.max(max, Math.ceil(prefixSum / (i+1)));
-            }
-            
-        }
-        
-        return (int) max;
-    }
 
+    public int minimizeArrayValue(int[] nums) {
+        int result = 0;
+        long[] dp = new long[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + dp[i - 1];
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            long ave = dp[i] / (long) (i + 1);
+            if (dp[i] % (i + 1) == 0) {
+                result = Math.max(result, (int) ave);
+            } else {
+                result = Math.max(result, (int) ave + 1);
+            }
+        }
+        return result;
+    }
 }
