2e940c76500c64ce9fc9edd446d1b00139650eea
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-04-07 14:59:48 +0530

    Time: 0 ms (100.00%), Space: 39 MB (91.39%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,22 +1,14 @@
 class Solution {
 
     public int bitwiseComplement(int num) {
-        // Step 1: Convert the integer to binary representation
-        String binary = Integer.toBinaryString(num);
-
-        // Step 2: Flip all the 0's to 1's and 1's to 0's in the binary representation
-        StringBuilder complementBuilder = new StringBuilder();
-        for (char c : binary.toCharArray()) {
-            if (c == '0') {
-                complementBuilder.append('1');
-            } else {
-                complementBuilder.append('0');
-            }
+        if(num==0)
+            return 1;
+        int complement = ~num;  // flip all bits of the original number
+        int bit = 1;
+        while (num != 0) {
+            num >>= 1;
+            bit <<= 1;
         }
-
-        // Step 3: Convert the binary complement back to an integer and return it
-        String complementBinary = complementBuilder.toString();
-        int complement = Integer.parseInt(complementBinary, 2);
-        return complement;
+        return complement & (bit - 1);  // mask out the extra 1's in the complement
     }
 }
