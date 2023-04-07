6e17b0e3471d9f422e87d2aaf4a1cc0e15ecee55
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-04-07 15:05:13 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (40.42%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -3,12 +3,14 @@ class Solution {
     public int bitwiseComplement(int num) {
         if(num==0)
             return 1;
-        int complement = ~num;  // flip all bits of the original number
-        int bit = 1;
-        while (num != 0) {
-            num >>= 1;
-            bit <<= 1;
-        }
-        return complement & (bit - 1);  // mask out the extra 1's in the complement
+        
+        int complement = ~num; 
+        // flip all bits of the original number
+        
+        int bit = Integer.highestOneBit(num) << 1; 
+        // get the leftmost set bit and shift it one position to the left
+
+        return complement & (bit - 1); 
+        // mask out the extra 1's in the complement
     }
 }
