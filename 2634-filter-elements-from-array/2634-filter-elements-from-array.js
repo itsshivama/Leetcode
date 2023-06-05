a6cf0960feada746406fd1b5752ab323db7a75d0
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2023-06-05 09:34:35 +0530

    Time: 63 ms (28.98%), Space: 42.1 MB (33.18%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -1,14 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
 var filter = function(arr, fn) {
-    let size = 0;
-    for (let i = 0; i < arr.length; ++i) {
-        if (fn(arr[i], i)) {
-            arr[size] = arr[i];
-            size++;
+    const res = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            res.push(arr[i]);
         }
-    }
-    // Ensure array is of length size
-    while (arr.length > size) {
-        arr.pop();
-    }
-    return arr
+    }  
+    return res;
 };
