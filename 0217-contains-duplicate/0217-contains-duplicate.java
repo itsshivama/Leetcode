69df698754168e8e115226785dd09eac1ae9c6fd
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2024-02-16 15:32:24 +0530

    Time: 7 ms (98.85%), Space: 60.3 MB (32.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num: nums){
-            if(map.containsKey(num))
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            if(!set.add(num))
                 return true;
-            map.put(num, map.getOrDefault(num, 0)+1);
         }
         return false;
     }
 }
     
 //     Brute Force
-    
+
 //             for(int i=0; i<nums.length; i++){
 //             for(int j=i+1; j<nums.length; j++){
 //                 if(nums[i]==nums[j])
@@ -20,3 +19,13 @@ class Solution {
 //         }
 //         return false;
 //     }
+
+// HashMap Method
+
+// HashMap<Integer, Integer> map = new HashMap<>();
+// for(int num: nums){
+//     if(map.containsKey(num))
+//         return true;
+//     map.put(num, map.getOrDefault(num, 0)+1);
+// }
+// return false;
