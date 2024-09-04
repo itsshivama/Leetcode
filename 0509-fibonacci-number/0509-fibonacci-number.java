6da21afd99ccc7a756ec5e51bf13245289223309
Shivam Agrawal <92708149+itsshivama@users.noreply.github.com>
2024-09-04 11:05:13 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (67.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,8 @@
 class Solution {
     public int fib(int n) {
         int sum = 1, slast=1;
-        if (n == 0)
-            return 0;
-        else if(n==2)
-            return 1;
+        if (n < 2)
+            return n;
         else{
             for(int i=3; i<=n; i++){
                 int temp = sum;
